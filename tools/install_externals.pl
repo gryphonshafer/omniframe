c00ef4c77e4899f2943516c56da6c7fbd54d50b3
Gryphon Shafer <gryphon@goldenguru.com>
2022-08-31 09:42:00 -0700

    Update external installs tool for Google Icons

--- tools/install_externals.pl
+++ tools/install_externals.pl
@@ -85,6 +85,72 @@ if ( $ext_yaml->{google_fonts} ) {
             join( "\n\n", @font_face_css_blocks ) . "\n"
         );
     }
+
+    if ( my $icons = $ext_yaml->{google_fonts}{icons} ) {
+        $ua->transactor->name( $icons->{browser} );
+        my $save_to = $dest_fonts->child('material-icons')->make_path;
+        my ( @font_face_css_blocks, @font_style_css_blocks );
+
+        for my $icon_name ( @{ $icons->{types} } ) {
+            my ($icon_url) = $ua->get(
+                'https://fonts.googleapis.com/icon', form => { family => $icon_name },
+            )->result->body =~ /src:\s*url\(([^\)]+)\)/;
+
+            my ($version) = $icon_url =~ m|/(v\d+)/|;
+            my ($format)  = $icon_url =~ /\.(\w+)$/;
+            ( my $icon_key = lc $icon_name ) =~ s/\s+/\-/g;
+
+            $ua->get($icon_url)->result->save_to(
+                $save_to->child( $icon_key . '-' . $version . '.' . $format )->to_string
+            );
+
+            push(
+                @font_face_css_blocks,
+                join(
+                    "\n",
+                    q\@font-face {\,
+                    qq\    font-family : '$icon_name';\,
+                    q\    font-style  : normal;\,
+                    q\    font-weight : 400;\,
+                    qq\    src         : local('$icon_name'),\,
+                    sprintf(
+                        ' ' x 18 . q\url('fonts/%s/%s-%s.%s') format('%s');\,
+                        'material-icons',
+                        $icon_key,
+                        $version,
+                        ($format) x 2,
+                    ),
+                    q\}\,
+                ),
+            );
+
+            push(
+                @font_style_css_blocks,
+                join(
+                    "\n",
+                    qq\.$icon_key {\,
+                    qq\    font-family                : '$icon_name';\,
+                    q\    font-weight                : normal;\,
+                    q\    font-style                 : normal;\,
+                    q\    font-size                  : 24px;\,
+                    q\    line-height                : 1;\,
+                    q\    letter-spacing             : normal;\,
+                    q\    text-transform             : none;\,
+                    q\    display                    : inline-block;\,
+                    q\    white-space                : nowrap;\,
+                    q\    word-wrap                  : normal;\,
+                    q\    direction                  : ltr;\,
+                    q\    -moz-font-feature-settings : 'liga';\,
+                    q\    -moz-osx-font-smoothing    : grayscale;\,
+                    q\}\,
+                ),
+            );
+        }
+
+        $dest_css->child('material-icons.css')->spurt(
+            join( "\n\n", @font_face_css_blocks, @font_style_css_blocks ) . "\n"
+        );
+    }
 }
 
 if ( $ext_yaml->{vue} ) {
@@ -163,79 +229,47 @@ This is the relative path to the external resources YAML file.
 This is the new project's root directory. If not defined, it defaults to "."
 or the current directory. If it doesn't exist, this directory will be created.
 
+=head1 USAGE DETAILS
 
+Review the default C<config/externals.yaml> file for a comprehensive example.
 
+=head2 Google Fonts
 
+For Google Fonts, use C<https://fonts.google.com> to search for fonts, then use
+C<https://google-webfonts-helper.herokuapp.com/fonts> to help sugggest settings
+for the YAML.
 
+To deploy a Google Font, include the generated CSS (stipulated in the
+C<google_fonts/dest/css> setting) in the CSS build. Then add some amount of CSS
+to connect the font definition to selectors. For example:
 
+    * {
+        font-family : 'Roboto Normal', sans-serif;
+    }
 
+    h1, h2, h3, h4, h5, h6, b, strong {
+        font-family : 'Roboto Bold';
+        font-weight : normal;
+    }
 
----
-google_fonts:
-    dest:
-        fonts : static/fonts
-        css   : config/assets/css/fonts
-    fonts:
-        Roboto:
-            variants:
-                'Roboto Bold'   : 500
-                'Roboto Italic' : italic
-                'Roboto Normal' : regular
-            subsets:
-                - latin
-            formats:
-                - woff2
-vue:
-    dest: static/js/vue
-    libs:
-        unpkg.com/vue/dist/vue.global.js                    : vue.js
-        unpkg.com/vue/dist/vue.global.prod.js               : vue.min.js
-        unpkg.com/vuex/dist/vuex.global.js                  : vuex.js
-        unpkg.com/vuex/dist/vuex.global.prod.js             : vuex.min.js
-        unpkg.com/vue-router/dist/vue-router.global.js      : vue-router.js
-        unpkg.com/vue-router/dist/vue-router.global.prod.js : vue-router.min.js
-font_awesome:
-    dest: static/font-awesome
-    parts:
-        - webfonts
-        - css/all.css
-        - css/all.min.css
+    i, em {
+        font-family : 'Roboto Italic';
+        font-style  : normal;
+    }
 
+    dl dt {
+        font-family : 'Roboto Bold';
+        font-weight : normal;
+    }
 
+=head2 Google Icons
 
+For Google Icons, the C<browser> setting is required because Google will use it
+to determine the font format to return. Setting this to a modern browser should
+result in the C<woff2> format.
 
+To deploy Google Icons, include the generated CSS (stipulated in the
+C<google_fonts/dest/css> setting) in the CSS build. Then add add HTML as needed.
+For example:
 
-
-
-
-* {
-    font-family : 'Roboto Normal', sans-serif;
-}
-
-h1, h2, h3, h4, h5, h6, b, strong {
-    font-family : 'Roboto Bold';
-    font-weight : normal;
-}
-
-i, em {
-    font-family : 'Roboto Italic';
-    font-style  : normal;
-}
-
-dl dt {
-    font-family : 'Roboto Bold';
-    font-weight : normal;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <span class="material-icons-outlined">done</span>
