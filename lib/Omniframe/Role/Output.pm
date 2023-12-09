5a5326bdbf10627bb5bbe40d6f8dcf49e12b8039
Gryphon Shafer <gryphon@goldenguru.com>
2023-12-08 17:05:01 -0800

    Add Omniframe::Class::Javascript

---
+++ lib/Omniframe/Role/Output.pm
@@ -0,0 +1,56 @@
+package Omniframe::Role::Output;
+
+use exact -role;
+use Data::Printer return_value => 'dump', colored => 1;
+
+sub dp ( $self, $params, @np_settings ) {
+    return map {
+        ( ref $_         ) ? "\n" . np( $_, @np_settings ) . "\n" :
+        ( not defined $_ ) ? '>undef<'                            :
+        ( $_ eq ''       ) ? '""'                                 : $_
+    } @$params;
+}
+
+sub deat ( $self, $error ) {
+    $error =~ s/\s+at\s.+\sline\s\d+\.\s*$//g;
+    return $error;
+}
+
+1;
+
+=head1 NAME
+
+Omniframe::Role::Output
+
+=head1 SYNOPSIS
+
+    package Package;
+
+    use exact -class;
+
+    with 'Omniframe::Role::Output';
+
+    sub method ($self) {
+        say $self->dp( { answer => 42 } );
+        say $self->deat('Something bad happened at /some/place.pl line 42.');
+
+        return;
+    }
+
+=head1 DESCRIPTION
+
+This role provides some output methods.
+
+=head1 METHODS
+
+=head2 dp
+
+This method accepts data along with an optional set of settings useful for
+L<Data::Printer>'s C<np> method.
+
+    say $self->dp( { answer => 42 }, @np_settings );
+
+=head2 deat
+
+This method removes any "at /some/place.pl line 42." instances from the end of
+any string passed in.
--- t/unit/Omniframe/Role/Logging.t
+++ t/unit/Omniframe/Role/Logging.t
@@ -6,7 +6,7 @@ my $obj;
 ok( lives { $obj = Omniframe->with_roles('+Logging')->new }, q{with_roles('+Logging')->new} ) or note $@;
 DOES_ok( $obj, "Omniframe::Role::$_" ) for ( qw( Conf Logging ) );
 can_ok( $obj, qw(
-    log_file log_level log_levels log_dispatch dp
+    log_file log_level log_levels log_dispatch
     debug info notice warning warn error err critical crit alert emergency emerg
 ) );
 
